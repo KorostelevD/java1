e50a3fa361397004d2c86c4dcc4102f6bcf5cc94
Dmytro Korostelyov <m0501543116@gmail.com>
2024-08-12 21:16:04 +0300

    Init

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        String s = scanner.nextLine();
+//        System.out.println(s);
+
+        int i = scanner.nextInt();
+        System.out.println(i+100);
+        System.out.println();
+        System.out.print(1);
+        System.out.print(2);
+        System.err.println(123);
+
+
+    }
+
+}
+
